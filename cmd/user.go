708a2846ab107069112cef179dec70b1987b6688
max.weis <max.weis@openknowledge.de>
2019-11-22 22:00:27 +0100

    remove init func

--- cmd/user.go
+++ cmd/user.go
@@ -9,15 +9,3 @@ var userCmd = &cobra.Command{
 	Short: "Manage users",
 	Long:  "Long",
 }
-
-func init() {
-	//if _, ok := os.LookupEnv(usernameKey); !ok {
-	//	fmt.Printf("%s is not set but is required to work.\n", usernameKey)
-	//	os.Exit(1)
-	//}
-	//
-	//if _, ok := os.LookupEnv(passwordKey); !ok {
-	//	fmt.Printf("%s is not set but is required to work.\n", passwordKey)
-	//	os.Exit(1)
-	//}
-}
